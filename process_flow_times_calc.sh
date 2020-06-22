c9b3924ad1431b33dee0e48a1ea39480d44aa95b
DDZERO9999 <38707835+DDZERO9999@users.noreply.github.com>
2020-06-22 13:21:51 -0600

    Add files via upload

--- process_flow_times_calc.sh
+++ process_flow_times_calc.sh
@@ -1,22 +1,59 @@
 #!/bin/sh -x
 
-sleep 1
-
+#################################
+#Modified 06/17/2020 Dave DeSalvo
+#################################
+###########VARIABLES#############
 #SET OUTPUT FILE
 OUTFILE=$AW_HOME/out/long_runner.$jobid.csv
 
-#FUNCTIONS
+#SET POSITIONAL VARIABLES
+PF_BL_RUNID=/tmp/process_flow_backlog.$1
+PF_TIMES_RUNID=/$AW_HOME/out/process_flow_times.$2
+PF_BL_LATE_RUNID=/tmp/process_flow_backlog_late.$3
 
-#REMOVE TMP FILES
+######FUNCTIONS##########
 REMOVE()
 	{
-		rm /tmp/process_flow_backlog.$1
-		rm /tmp/process_flow_backlog_late.$2
+		#REMOVE TMP FILES
+		rm $PF_BL_RUNID
+		rm $PF_BL_LATE_RUNID
 	}
 
-#TURN HOURS INTO MINUTES FUNCTION 
+CHECK_FILES()
+	{
+		#FILE CHECK EXISTS
+		if [[ -e $PF_TIMES_RUNID ]] 
+			then
+				:
+		else 
+			echo "FILE $(ls $PF_TIMES_RUNID) DOES NOT EXIST"
+			REMOVE
+			exit 1
+		fi
+		#FILE CHECK EXISTS
+		if [[ -e $PF_BL_RUNID ]]
+			then
+				: 
+		else 
+			echo "FILE $(ls $PF_BL_RUNID) DOES NOT EXIST"
+			REMOVE
+			exit 1
+		fi
+		#FILE CHECK EXISTS
+		if [[ -e $PF_BL_LATE_RUNID ]]
+			then
+				: 
+		else 
+			echo "FILE $(ls $PF_BL_LATE_RUNID) DOES NOT EXIST"
+			REMOVE
+			exit 1
+		fi
+	}
+
 CALC_INTERVAL()
 	{
+		#TURN HOURS INTO MINUTES FUNCTION
 		if [[ $FREQUENCY = "H" ]]
 			then 
 				((I = $INTERVAL * 60))
@@ -25,11 +62,10 @@ CALC_INTERVAL()
 				:
 		fi
 	}
-
-#Calc minutes to hours for elapsed 
+ 
 CALC_MIN_TO_HR_ELAPS()
 	{
-		
+		#Calc minutes to hours for elapsed
 		if [[ $1 -gt 59 ]]
 			then 
 				((ELAPSED = $1 / 60))
@@ -39,9 +75,9 @@ CALC_MIN_TO_HR_ELAPS()
 		fi
 	}
 
-#Calc minutes to hours for average
 CALC_MIN_TO_HR_AVG()
 	{
+		#Calc minutes to hours for average
 		AVG=$1
 		
 		if [[ $AVG -gt 59 ]]
@@ -53,9 +89,9 @@ CALC_MIN_TO_HR_AVG()
 		fi
 	}
 	
-#CALC IF PROCESS FLOW IS RUNNING LONG FUNCTION
 LONG_RUNNER()
 	{
+		#CALC IF PROCESS FLOW IS RUNNING LONG FUNCTION
 		((z = $1 * 3 + $2))
 		if [[ $z -lt 10 ]]
 			then
@@ -72,133 +108,81 @@ LONG_RUNNER()
 		fi
 	}		
 
-EXCEPTIONS()
+LOOP_TIMES_FILE()
 	{
-		PROCESS_FLOW=$1; SD=$2
-		
-		if [[ $PROCESS_FLOW = "UTY_GBL_SALES_RECURRING_1MIN" ]]
-			then ((k = $2 + 100))
-			STAND_DEV=$k
-		fi
-	}
-
-#BODY
-	
-#FILE CHECK EXISTS
-if [ -e /$AW_HOME/out/process_flow_times.$2 ] 
-	then
-		:
-	else 
-		echo "FILE `ls /$AW_HOME/out/process_flow_times.$2` DOES NOT EXIST";
-		REMOVE $1 $3
-		exit 1
-fi
-
-#FILE CHECK EXISTS
-if [ -e /tmp/process_flow_backlog.$1 ]
-	then
-		: 
-	else 
-		echo "FILE `ls /tmp/process_flow_backlog.$1` DOES NOT EXIST"
-		REMOVE $1 $3
-		exit 1
-fi
-
-#FILE CHECK EXISTS
-if [ -e /tmp/process_flow_backlog_late.$3 ]
-	then
-		: 
-	else 
-		echo "FILE `ls /tmp/process_flow_backlog_late.$3` DOES NOT EXIST"
-		REMOVE $1 $3
-		exit 1
-fi
-
-#Create output file, Set column header
-echo "PROCESS FLOWS, AVERAGE, ELAPSED, FREQUENCY, RUN ID, STATUS" > $OUTFILE
-
-#CREATE PROCESSING FILE
-while read LINE 
-	do 
 		#SET VARIABLES
 		PF=$(echo $LINE | awk '{print $1}')
-		FILE=$(echo $LINE | awk '{print $1, $4, $5, $6, $7}') #PROCESS_FLOW, ELAPSED, FREQUENCY(D H M), INTERVAL(MIN), RUNID
-		TIMES=$(cat /$AW_HOME/out/process_flow_times.$2 | grep -w $PF | awk '{print $4, $6}') #AVERAGE, STAND_DEV
+		FILE=$(echo $LINE | awk '{print $1, $4, $5, $6, $7, $8}') #PROCESS_FLOW, ELAPSED, FREQUENCY(D H M), INTERVAL(MIN), RUNID, STATUS
+		TIMES=$(cat $PF_TIMES_RUNID | grep -w $PF | awk '{print $4, $6}') #AVERAGE, STAND_DEV
 		COMBO=$(echo "$FILE $TIMES") # 1 Process_flow, 2 ELAPSED, 3 FREQUENCY, 4 INTERVAL, 5 RUNID, 6 AVERAGE, 7 STAND_DEV
 		ELAPSED=$(echo $COMBO  | awk '{print $2}'); FREQUENCY=$(echo $COMBO  | awk '{print $3}')
-		INTERVAL=$(echo $COMBO  | awk '{print $4}'); RUNID=$(echo $COMBO  | awk '{print $5}')
-		AVERAGE=$(echo $COMBO  | awk '{print $6}'); STAND_DEV=$(echo $COMBO  | awk '{print $7}')
-		#echo $PF $ELAPSED $FREQUENCY $INTERVAL $RUNID $AVERAGE $STAND_DEV
-		
+		INTERVAL=$(echo $COMBO  | awk '{print $4}'); RUNID=$(echo $COMBO  | awk '{print $5}'); STATUS=$(echo $COMBO  | awk '{print $6}')
+		AVERAGE=$(echo $COMBO  | awk '{print $7}'); STAND_DEV=$(echo $COMBO  | awk '{print $8}')
+
+		#Makes Averate and Standard Deviation zero where there is no value
 		if [[ -z $AVERAGE || -z $STAND_DEV ]]
 			then
 				AVERAGE=0
 				STAND_DEV=0
 		fi
-		
-		#PROCESS 
-		#EXCEPTIONS $PF $STAND_DEV 
-		#update hour interval
-				
-		CALC_INTERVAL $FREQUENCY $INTERVAL
-				
-		#calc long runners
-		LONG_RUNNER $STAND_DEV $AVERAGE $ELAPSED
-					
-		#remove unwanted entries
-		if [[ -z $STATUS ]] || [[ $AVERAGE -eq 0 ]]
-			then :
-			else #REMOVE CHECK TO SEE IF MINUTES and HOURS unless it is over frequency
-				if [[ $FREQUENCY != M ]] || [[ $FREQUENCY != H ]]
-					then 
-						CALC_MIN_TO_HR_ELAPS $ELAPSED
-						CALC_MIN_TO_HR_AVG $AVERAGE
-						RESPONSE=$(echo "$PF,$AVERAGE,$ELAPSED,$FREQUENCY,$RUNID,$STATUS")
-						echo "$RESPONSE" >> $OUTFILE
-					else [[ $FREQUENCY = M || $FREQUENCY = H && $ELAPSED -gt $INTERVAL ]]
+	}
+
+PROCESS_BACKLOG_FILE()
+	{
+	while read LINE 
+		do 
+			LOOP_TIMES_FILE 				
+			CALC_INTERVAL $FREQUENCY $INTERVAL				
+			LONG_RUNNER $STAND_DEV $AVERAGE $ELAPSED				
+			#remove unwanted entries
+			if [[ -z $STATUS ]] || [[ $AVERAGE -eq 0 ]]
+				then :
+				else #REMOVE CHECK TO SEE IF MINUTES and HOURS unless it is over frequency
+					if [[ $FREQUENCY = M || $FREQUENCY = H && $ELAPSED -gt $INTERVAL ]]
+						then
 							CALC_MIN_TO_HR_ELAPS $ELAPSED
 							CALC_MIN_TO_HR_AVG $AVERAGE
 							RESPONSE=$(echo "$PF,$AVERAGE,$ELAPSED,$FREQUENCY,$RUNID,$STATUS")
 							echo "$RESPONSE" >> $OUTFILE
-				fi
-		fi	
-	done < /tmp/process_flow_backlog.$1  
+					elif [[ $FREQUENCY != M || $FREQUENCY != H ]]
+						then	
+							CALC_MIN_TO_HR_ELAPS $ELAPSED
+							CALC_MIN_TO_HR_AVG $AVERAGE
+							RESPONSE=$(echo "$PF,$AVERAGE,$ELAPSED,$FREQUENCY,$RUNID,$STATUS")
+							echo "$RESPONSE" >> $OUTFILE
+					fi
+			fi	
+		done < $PF_BL_RUNID  
+	}
 
-#Late starting process flows
+PROCESS_LATE_FILE()
+	{
+	while read LINE 
+		do 
+			LOOP_TIMES_FILE
+			CALC_MIN_TO_HR_ELAPS $ELAPSED
+			CALC_MIN_TO_HR_AVG $AVERAGE
+			#Set echo response
+			RESPONSE=$(echo "$PF,$AVERAGE,$ELAPSED,$FREQUENCY,$RUNID,$STATUS LONG_")		
+			echo $RESPONSE >> $OUTFILE		
+		done < $PF_BL_LATE_RUNID 
+	}
 
-while read LINE 
-	do 
-		#SET VARIABLES
-		PF=$(echo $LINE | awk '{print $1}')
-		FILE=$(echo $LINE | awk '{print $1, $4, $5, $6, $7, $8}') #PROCESS_FLOW, ELAPSED, FREQUENCY(D H M), INTERVAL(MIN), RUNID, STATUS
-		TIMES=$(cat /$AW_HOME/out/process_flow_times.$2 | grep -w $PF | awk '{print $4, $6}') #AVERAGE, STAND_DEV
-		COMBO=$(echo "$FILE $TIMES") # 1 Process_flow, 2 ELAPSED, 3 FREQUENCY, 4 INTERVAL, 5 RUNID, 6 STATUS 7 AVERAGE, 8 STAND_DEV
-		ELAPSED=$(echo $COMBO  | awk '{print $2}'); FREQUENCY=$(echo $COMBO  | awk '{print $3}')
-		INTERVAL=$(echo $COMBO  | awk '{print $4}'); RUNID=$(echo $COMBO  | awk '{print $5}'); STATUS=$(echo $COMBO  | awk '{print $6}')
-		AVERAGE=$(echo $COMBO  | awk '{print $7}'); STAND_DEV=$(echo $COMBO  | awk '{print $8}')
-		#echo $PF $ELAPSED $FREQUENCY $INTERVAL $RUNID $STATUS $AVERAGE $STAND_DEV 
-		
-		if [[ -z $AVERAGE || -z $STAND_DEV ]]
-			then
-				AVERAGE=0
-				STAND_DEV=0
-		fi
+REGISTER_FILE()
+	{
+		file=$OUTFILE
+		$AW_HOME/exec/FILESIZE $file $err
+	}
 
-		CALC_MIN_TO_HR_ELAPS $ELAPSED
-		CALC_MIN_TO_HR_AVG $AVERAGE
-		
-		#Set echo response
-		RESPONSE=$(echo "$PF,$AVERAGE,$ELAPSED,$FREQUENCY,$RUNID,$STATUS LONG_")
-		
-		echo $RESPONSE >> $OUTFILE
-		
-		
-	done < /tmp/process_flow_backlog_late.$3 
+MAIN()
+	{
+		CHECK_FILES
+		#CREATE CSV FILE, Set column header
+		echo "PROCESS FLOWS, AVERAGE, ELAPSED, FREQUENCY, RUN ID, STATUS" > $OUTFILE 
+		PROCESS_BACKLOG_FILE #LONG RUNNERS
+		PROCESS_LATE_FILE #WAITING TO RUN
+		REGISTER_FILE #REGISTER FILE
+		REMOVE #REMOVE TMP FILES
+	}
 
-
-#set registered files
-file=$OUTFILE
-$AW_HOME/exec/FILESIZE $file $err
-sleep 1 
-#remove files from tmp
-REMOVE $1 $3
+MAIN
